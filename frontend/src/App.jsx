6c3a89639ff38eb2cd3990d32e03355561ec860a
ali <mrrizwanaali@gmail.com>
2025-08-29 17:14:46 +0500

    Initial MERN + Vite setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="bg-gray-700 text-5xl text-black font-medium">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
